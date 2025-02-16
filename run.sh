a5604dd0e6d12295cdef5dc1d1d6bfaa405c5e81
matheus de faria <mattheusdefaria@proton.me>
2025-02-16 00:05:25 -0300

    sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# rzip.sh
+#
+# Copia o programa rzip para a pasta /usr/bin.
+
+# O programa lista e descomprimi arquivos zip. 
+
+# Versão 1.0: Rzip
+#	      Suporta a opção [-h]
+#
+# Matheus de Faria, Fevereiro de 2025
+
+# processamento do programa
+
+sudo cp target/release/rzip /usr/bin
