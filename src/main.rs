dd9484ea1c08027f763654f41215f191ef28761b
matheus de faria <mattheusdefaria@proton.me>
2025-02-15 22:07:35 -0300

    adicionando mais funções

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{fs, io, path::Path,env};
+
 fn main() {
-    println!("Hello, world!");
+    // Chama a função para extair arquivos 
+    std::process::exit(extrair_arquivos());
+}
+
+
+// Função para extrair arquivos
+fn extrair_arquivos() -> i32 {
+    let nome_arquivo = "arq.zip";
+    let path_arq = Path::new(&nome_arquivo);
+
+    let mut arq = fs::File::open(&path_arq).unwrap();
+    
+    let mut arq_zip = zip::ZipArchive::new(arq).expect("Erro ao ler zip");
+
+    for x in 0..arq_zip.len() {
+        println!("{x}");
+    }
+    0
+}
+
+// Função para extrair arquivos dentro do arquivo zip
+fn listar_arquivos() {
+    todo!()
+}
+
+// Função de ajuda
+fn help() {
+    todo!()
 }
