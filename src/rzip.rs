4faa15edb4e893ee97b0796ba5c42ed84085bd45
matheus de faria <mattheusdefaria@proton.me>
2025-02-16 01:18:14 -0300

    update

--- src/rzip.rs
+++ src/rzip.rs
@@ -4,17 +4,35 @@ pub mod rzip {
     pub fn extrair_arquivos(nome: &std::fs::File) -> i32 {
         let mut arq_zip = zip::ZipArchive::new(nome).expect("Erro ao ler zip");
 
+        // Imprimi na tela todos os arquivos dentro do arquivo zip
         for x in 0..arq_zip.len() {
-            let nome_arqs = arq_zip.by_index(x).unwrap();
+            let mut nome_arqs = arq_zip.by_index(x).unwrap();
 
+            // Exibir o caminho dos arquivos
             let caminho_arq = match nome_arqs.enclosed_name() {
-                Some(path) => path.to_owned(),
+                Some(caminho) => caminho.to_owned(),
                 None => continue,
             };
 
+            // Verifica e cria diretórios
             if (*nome_arqs.name()).ends_with("/") {
                 println!("Arquivos extraidos para {}",caminho_arq.display());
                 std::fs::create_dir_all(&caminho_arq).unwrap();
+            }
+
+            else {
+                // Verifica e cria diretórios
+                if let Some(c) = caminho_arq.parent() {
+                    if !c.exists() {
+                        std::fs::create_dir_all(&c).unwrap();
+                    }
+                }
+
+                // Arquivo de saida
+                let mut arq_saida = std::fs::File::create(&caminho_arq).unwrap();
+
+                // Copia o conteúdo de nome_arqs para arq_saida
+                std::io::copy(&mut nome_arqs, &mut arq_saida).unwrap();
             }
         }
         0
@@ -30,7 +48,7 @@ pub mod rzip {
         
         // Imprimi na tela todos os arquivos dentro do arquivo zip
         for x in 0..arq_zip.len() {
-            let mut nome_arqs = arq_zip.by_index(x).unwrap();
+            let nome_arqs = arq_zip.by_index(x).unwrap();
             
             // Exibir os nomes dos arquivos
             match nome_arqs.enclosed_name() {
